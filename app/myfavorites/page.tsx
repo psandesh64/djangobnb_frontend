72e1393726706179960e497d62fa3962fd02f232
psandesh64 <psandesh64@gmail.com>
2024-10-24 19:19:37 +0545

    second commit

--- app/myfavorites/page.tsx
+++ app/myfavorites/page.tsx
@@ -16,12 +16,10 @@ const MyFavortiesPage = async () => {
     <main className="max-w-[1500px] mx-auto px-6 pb-12">
         <h1 className="my-6 text-2xl">My favorites</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <PropertyList
-                favorites={true}
-            />
+        <PropertyList
+            favorites={true}
+        />
     
-        </div>
     </main>
   )
 }
--- app/myproperties/page.tsx
+++ app/myproperties/page.tsx
@@ -8,10 +8,9 @@ const MyPropertiesPage = async () => {
   return (
     <main className='max-w-[1500px] mx-auto px-6 pb-6'>
         <h1 className='my-6 text-2xl'>My properties</h1>
-
         
-            <PropertyList
-              landlord_id={userId}/>
+        <PropertyList
+          landlord_id={userId}/>
       
     </main>
   )
